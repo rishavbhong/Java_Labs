6766e01b38adc7de67723c0afaaf27a765e3d9d9
rishavbhong <rishavkumar.jeh@gmail.com>
2024-09-12 02:06:31 +0530

    First commit

--- Main.java
+++ Main.java
@@ -1,7 +1,66 @@
 
-public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+
+import java.util.Scanner;
+
+class Calculator{
+    int Sum (int a, int b){
+        return (a+b);
     }
+    int Difference(int a, int b){
+        if (a>b){
+            return (a-b);
+        }
+        else{
+            return (b-a);
+        }
+    }
+    int Product(int a, int b){
+        return a*b;
+    }
+    float Quotient(int a, int b){
+        if (a>b){
+            return a/b;
+        }
+        else{
+            return b/a;
+        }
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter the Integers a & b space seperated: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println("1: Sum, 2: Difference, 3: Product, 4: Quotient");
+        int c = sc.nextInt();
+
+        int result;
+        float Qr;
+
+        Calculator Ca = new Calculator();
+
+        switch (c){
+            case 1:
+                result = Ca.Sum(a,b);
+                System.out.println(result);
+                break;
+            case 2:
+                result = Ca.Difference(a,b);
+                System.out.println(result);
+                break;
+            case 3:
+                result = Ca.Product(a,b);
+                System.out.println(result);
+                break;
+            case 4:
+                Qr = Ca.Quotient(a,b);
+                System.out.println(Qr);
+                break;
+        }
+
+    }
+
 }
